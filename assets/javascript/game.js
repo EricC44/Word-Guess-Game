3caee6352376510327985df9522ae8f31c40df5e
ECarlson <ericcarlson4@outlook.com>
2019-03-14 16:52:03 -0700

    Work on pushing letters into right and wrong guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,32 @@
-const words = ["Caustic" , "Lifeline" , "Gibraltar" , "Bloodhound" , "Mirage" , "Wraith" , "Bangalore" , "Pathfinder" , "Apex Legends" , "Mastiff" , "Peacekeeper" , "Spitfire" , "Wingman" , "Kraber" , "Longbow" , "Devotion" , "Hemlok" , "Havoc" , "Flatline" , "Triple Take" , "Alternator" , "Prowler" , "Mozambique" , "Syringe" , "Med Kit", "Phoenix Kit" , "Shield Cell" , "Shield Battery" , "Ultimate Accelerant" , "Body Shield" , "Helmet" , "Knockdown Shield" , "Backpack" , "Slum Lakes" , "The Pit" , "Runoff" , "Cascades" , "Artillery" , "Bunker" , "Air base" , "Relay" , "Wetlands" , "Bridges" , "Swamps" , "Hydro Dam" , "Repulsor" , "Water Treatment" , "Market" , "Skull Town" , "Thunderdome"];
-let randomWord = words[Math.floor(Math.random() * words.length)];
-console.log(randomWord)
+const words = ["Caustic" , "Lifeline" , "Gibraltar" , "Bloodhound" , "Mirage" , "Wraith" , "Bangalore" , "Pathfinder" , "ApexLegends" , "Mastiff" , "Peacekeeper" , "Spitfire" , "Wingman" , "Kraber" , "Longbow" , "Devotion" , "Hemlok" , "Havoc" , "Flatline" , "TripleTake" , "Alternator" , "Prowler" , "Mozambique" , "Syringe" , "MedKit", "PhoenixKit" , "ShieldCell" , "ShieldBattery" , "UltimateAccelerant" , "BodyShield" , "Helmet" , "KnockdownShield" , "Backpack" , "SlumLakes" , "ThePit" , "Runoff" , "Cascades" , "Artillery" , "Bunker" , "Airbase" , "Relay" , "Wetlands" , "Bridges" , "Swamps" , "HydroDam" , "Repulsor" , "WaterTreatment" , "Market" , "SkullTown" , "Thunderdome"];
+let randomNum = [Math.floor(Math.random() * words.length)];
+let pickedWord = words[randomNum];
+let underscore = [];
+let rightword = [];
+let wrongword = [];
+let underscore = [];
+const maxGuess = 10;
+
+console.log(pickedWord);
+
+let generateUnderscore = () => {
+    for(let i = 0; i < pickedWord.length; i++) {
+        underscore.push('_');
+
+    }
+    return underscore;
+}
+
+console.log(generateUnderscore());
+
+document.addEventListener('keypress' , (event) => {
+    console.log(event);
+    let keyword = String.fromCharCode(event.keyCode);
+    console.log(pickedWord.indexOf(keyword));
+    if(pickedWord.indexOf(keyword) > -1) {
+        console.log(true);
+    }
+    
+
+
+});
